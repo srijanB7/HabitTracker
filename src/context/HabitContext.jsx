cad29ae0ea0d696791e298706893132d1fe399d0
srijanbiswas7 <srijanbiswas955@gmail.com>
2023-06-18 21:06:56 +0530

    added a default habit

--- src/context/HabitContext.jsx
+++ src/context/HabitContext.jsx
@@ -3,35 +3,51 @@ import { createContext, useState } from "react";
 export const HabitContext = createContext();
 
 export const HabitProvider = ({ children }) => {
-    const [habits, setHabits] = useState([]);
+    const [habits, setHabits] = useState([{
+        id: 1,
+        name: "Learn React",
+        timeOfDay: "Any Time",
+        time: "Today",
+        goal: "1 times Daily",
+        repeat: "Daily",
+    },]);
     const [archiveHabits, setArchiveHabits] = useState([]);
 
     const addHabit = (habitDetails) => {
-        setHabits([...habits, habitDetails])
-    }
+        setHabits([...habits, habitDetails]);
+    };
 
     const deleteHabit = (id) => {
-        setHabits(prev => prev.filter(habit => habit.id !== id));
-    }
+        setHabits((prev) => prev.filter((habit) => habit.id !== id));
+    };
 
     const archiveHabit = (id) => {
-        const habit = habits.find(habit => habit.id === id);
+        const habit = habits.find((habit) => habit.id === id);
         setArchiveHabits([...archiveHabits, habit]);
-        setHabits(prev => prev.filter(habit => habit.id !== id));
-    }
+        setHabits((prev) => prev.filter((habit) => habit.id !== id));
+    };
 
     const editHabit = (habitDetails, id) => {
-        const updatedHabit = habits.map(habit => {
-            if(habit.id === id) {
-                return habitDetails
+        const updatedHabit = habits.map((habit) => {
+            if (habit.id === id) {
+                return habitDetails;
             }
             return habit;
-        })
+        });
         setHabits(updatedHabit);
-    }
+    };
 
     return (
-        <HabitContext.Provider value={{ habits, addHabit, deleteHabit, archiveHabits, archiveHabit, editHabit }}>
+        <HabitContext.Provider
+            value={{
+                habits,
+                addHabit,
+                deleteHabit,
+                archiveHabits,
+                archiveHabit,
+                editHabit,
+            }}
+        >
             {children}
         </HabitContext.Provider>
     );
